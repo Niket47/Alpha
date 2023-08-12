03229502324dcca471ac750dc77f3be82e8dfbe6
Niket dadhaniya <116706204+Niket47@users.noreply.github.com>
2023-08-12 11:19:55 +0530

    las expense code with new practice app

---
+++ Exercise/PassData/FirstPass.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FirstPass = () => {
+  return (
+    <View>
+      <Text>FirstPass</Text>
+    </View>
+  )
+}
+
+export default FirstPass
